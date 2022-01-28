be336d0ee72cbbefa9621042e52d898387518d5a
keerthisasikumar20 <keerthisasikumar02@gmail.com>
2022-01-28 19:39:17 +0530

    Update Project Structure

--- src/test/java/org/test/SampleGitProgram.java
+++ src/test/java/org/test/SampleGitProgram.java
@@ -3,5 +3,7 @@ package org.test;
 public class SampleGitProgram {
 public static void main(String[] args) {
 	System.out.println("Think Positive");
+	System.out.println("Be Bold & Brave");
+	System.out.println("Hope And Say All Is Well");
 }
 }
