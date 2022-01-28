c22fa5a17201cc8832e838ac3a6b5849b75fb0c7
keerthisasikumar20 <keerthisasikumar02@gmail.com>
2022-01-28 20:59:32 +0530

    keerthi done her work

--- src/test/java/org/test/SampleGitProgram.java
+++ src/test/java/org/test/SampleGitProgram.java
@@ -1,6 +1,10 @@
 package org.test;
 
 public class SampleGitProgram {
+	private void hopeIsMust() {
+		System.out.println("Again And Again You Do Same Thing Is It Easy to You");
+
+	}
 public static void main(String[] args) {
 	System.out.println("Think Positive");
 	System.out.println("Be Bold & Brave");
